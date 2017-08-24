205e8a1e76704c08bf7f43215cbfd03b8ca9c421
Jordan Schuetz <schuetz@pubnub.com>
2017-08-24 13:58:35 -0700

    Update main.js

--- main.js
+++ main.js
@@ -18,27 +18,22 @@ pubnub.subscribe({
 listener = {
   status(response) {
     if (response.category === "PNConnectedCategory") {
-      // PNConnectedCategory confirmation that we have joined a channel
-      // to see which channel(s) we just joined, call response.affectedChannels.
-      // Now that we know we joined a channel(s), we need to find out
-      // who else is already subscribed to this channel(s) before we joined
       hereNow(response.affectedChannels);
     }
   },
   message(response) {
   },
   presence(response) {
-    //console.log(response)
     if (response.action === "join") {
       for(i=0; i < response.occupancy; i++){
         if(response.uuid !== undefined){
           var uuidMatchJoin = playerList.indexOf(response.uuid);
-          console.log("UUID ARRAY INDEX: ", uuidMatchJoin, "UUID: ", response.uuid)
+          console.log("UUID ARRAY INDEX: ", uuidMatchJoin, "UUID: ", response.uuid);
           if(uuidMatchJoin === -1){
             playerList[playerList.length] = response.uuid;
-            console.log("Insert ", response.uuid, "in array" )
+            console.log("Insert ", response.uuid, "in array" );
           } else {
-            console.log("UUID: ", response.uuid, "is already in the array")
+            console.log("UUID: ", response.uuid, "is already in the array");
           }
         }
       }
@@ -62,8 +57,8 @@ listener = {
         for(i=0; i < response.occupancy; i++){
           var uuidMatchIntervalLeave = playerList.indexOf(response.leave[i]);
           if(uuidMatchIntervalLeave > -1){
-            console.log("REMOVE PLAYER FROM ARRAY", uuidMatchIntervalLeave)
-            playerList.splice(uuidMatchIntervalLeave, 1)
+            console.log("REMOVE PLAYER FROM ARRAY", uuidMatchIntervalLeave);
+            playerList.splice(uuidMatchIntervalLeave, 1);
           }
         }
       }
@@ -73,23 +68,20 @@ listener = {
       for(i=0; i < response.occupancy; i++){
         var uuidMatchLeave = playerList.indexOf(response.uuid);
         if(uuidMatchLeave > -1){
-          console.log("REMOVE PLAYER FROM ARRAY", uuidMatchLeave, "with UUID: ", response.uuid)
-          playerList.splice(uuidMatchLeave, 1)
+          console.log("REMOVE PLAYER FROM ARRAY", uuidMatchLeave, "with UUID: ", response.uuid);
+          playerList.splice(uuidMatchLeave, 1);
         }
       }
     }
-    console.log("Presence UUIDs:", playerList)
+    console.log("Presence UUIDs:", playerList);
   },
 }
 
+pubnub.addListener(listener);
+
 function hereNow(channels) {
-  // clearInterval(occupancyCounter)
-
-  // you may have joined more than one channel so you would need to
-  // iterate on channels and call hereNow for each one
-  for (channel in channels) {
-    var channel = channels[0];
-
+  for (i in channels) {
+    var channel = channels[i];
     pubnub.hereNow(
     {
       channel: channel,
@@ -97,22 +89,14 @@ function hereNow(channels) {
       includeState: true
     },
     function (status, response) {
-      // keep in mind that due to the "eventual consistance" reality of
-      // realtime distributed network computing, you may not see yourself
-      // in this list of occupants returned by the hereNow call.
-      // but we know we are subscribed and it is just a matter of
-      // event consistency to catch up in a matter of milliseconds to possibly seconds
       console.log("hereNow Response: ", response);
       for(i=0; i < response.totalOccupancy; i++){
         playerList[i] = response.channels.playerlobby.occupants[i].uuid;
       }
-      console.log("hereNow UUIDs: ", playerList)
-      pubnub.addListener(listener);
+      console.log("hereNow UUIDs: ", playerList);
     });
-  } // for each
+  }
 }
-
-// occupancyCounter = setInterval(hereNow, 2000);
 
 // If person leaves or refreshes the window, run the unsubscribe function
 onbeforeunload = function() {
